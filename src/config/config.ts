8e2bc69561fa7889ea318eb97e3ab5990432ed14
tuhin <echo>
2024-02-29 10:29:10 +0530

    filter fixed

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,8 +2,8 @@ import { io } from "socket.io-client";
 
 // export const url ="http://mmProd-1434899310.ap-south-1.elb.amazonaws.com"
 // export const url ="mmProd-1434899310.ap-south-1.elb.amazonaws.com"
-export const url = "http://192.168.1.44"
-export const port = 8989
+export const url = "http://65.1.183.77"
+export const port = 8181
 export const version = "v1"
 
 export const headers = {
