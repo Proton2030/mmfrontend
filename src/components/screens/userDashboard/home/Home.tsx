351bde174b7e87aaa89bab070bc61f8bac8fb9bf
tuhin <echo>
2024-03-02 11:19:49 +0530

    new filter drawer added

--- src/components/screens/userDashboard/home/Home.tsx
+++ src/components/screens/userDashboard/home/Home.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useContext, useEffect, useRef, useState } from 'react';
-import { View, ActivityIndicator, FlatList, NativeSyntheticEvent, NativeScrollEvent, RefreshControl, Text, Modal, ScrollView } from 'react-native';
+import { View, ActivityIndicator, FlatList, NativeSyntheticEvent, NativeScrollEvent, RefreshControl, Text, Modal, ScrollView, StyleSheet, Animated, Easing, TouchableOpacity } from 'react-native';
 import { Appbar, Badge, Button, Checkbox, IconButton, Portal, Searchbar, Tooltip } from 'react-native-paper';
 import { useNavigation } from '@react-navigation/native';
 import AuthContext from '../../../../contexts/authContext/authContext';
@@ -10,12 +10,11 @@ import { IUserDetails } from '../../../../@types/types/userDEtails.types';
 import { shuffleArray } from '../../../../utils/commonFunction/suffleArray';
 import _ from 'lodash';
 import SmallLoader from '../../../shared/smallLoader/SmallLoader';
-import Slider from '@react-native-community/slider';
-import MultiSlider from '@react-native-community/slider';
 import { handelVibrate } from '../../../../utils/commonFunction/systemvibration';
 import axios from 'axios'; // Import axios
-import { getFilterList } from '../../../../utils/api/filter/filter';
 import { MessageSeenCountContext } from '../../../../contexts/messageSeenContext/MessageSeenCountContextProvider';
+import FilterDrawer from '../../../shared/filterDrawer/FilterDrawer';
+import { globalStyles } from '../../../../globalStyles/GlobalStyles';
 
 const Home = () => {
 
@@ -35,26 +34,35 @@ const Home = () => {
     const [hasSalah, setHasSalah] = useState<string>("");
     const [hasSawm, setHasSawm] = useState<string>("");
     const [sliderValue, setSliderValue] = useState<number>(80);
+    const [drawerVisible, setDrawerVisible] = useState(false);
+    const [animation] = useState(new Animated.Value(0));
+    const [showOverlay, setShowOverlay] = useState(false);
 
-    const handleSliderChange = (value: any) => {
-        setSliderValue(Math.round(value));
-    };
-
-    const handleRefreshFilters = () => {
-        setMaritalStatus('');
-        setHasSalah('');
-        setHasSawm('');
-        setFilterModalVisible(false);
-        getSuggestionUserApi("REFRESH");
-    };
-
-    const getAgeRange = () => {
-        if (sliderValue <= 40) {
-            return `0 to ${sliderValue}`;
+    useEffect(() => {
+        if (drawerVisible) {
+            setTimeout(() => {
+                setShowOverlay(true);
+            }, 500);
         } else {
-            return `0 to 80`;
+            setShowOverlay(false);
         }
+    }, [drawerVisible]);
+
+    const toggleDrawer = () => {
+        setDrawerVisible(!drawerVisible);
+        Animated.timing(animation, {
+            toValue: drawerVisible ? 0 : 1,
+            duration: 300,
+            easing: Easing.ease,
+            useNativeDriver: false,
+        }).start();
     };
+
+    const translateX = animation.interpolate({
+        inputRange: [0, 1],
+        outputRange: [300, 0],
+    });
+
     const flatListRef = useRef<any>(null);
     const handleScrollToTop = () => {
         if (flatListRef.current) {
@@ -63,30 +71,26 @@ const Home = () => {
         }
     };
 
-    const showFilterModal = () => {
-        setFilterModalVisible(true);
-    };
 
-    const hideFilterModal = async () => {
-        const gender = (user?.gender === "MALE") ? "FEMALE" : "MALE"
-        console.log("-------->filter api");
-
+    const hideFilterModal = async (filterOptions: { maritalStatus: any; hasSalah: any; hasSawm: any; }) => {
+        const gender = (user?.gender === "MALE") ? "FEMALE" : "MALE";
+        console.log(filterOptions)
         try {
             const params = {
                 gender: gender,
-                marital_status: maritalStatus,
-                salah: hasSalah,
-                sawum: hasSawm
+                marital_status: filterOptions?.maritalStatus,
+                salah: filterOptions?.hasSalah,
+                sawum: filterOptions?.hasSawm
             };
             const response = await api.filter.getFilterList(params);
             setSuggestedUser(response);
-            // setLoading(true);
-            setFilterModalVisible(false);
+            toggleDrawer()
         } catch (error) {
             console.error(error);
             // setLoading(false);
         }
     };
+
 
     const addChoice = useCallback(async (sender_id: string, reciver_id: string) => {
         const payload = {
@@ -130,11 +134,6 @@ const Home = () => {
             }
         }
     }
-
-    // const handleUnchoice = async (choiceId: string) => {
-    //     const response = await api.userChoice.unChoice(choiceId);
-    //     console.log("called")
-    // }
     const getSuggestionUser = useCallback(async () => {
         getSuggestionUserApi("REFRESH");
     }, [user, page]);
@@ -165,7 +164,12 @@ const Home = () => {
         getSuggestionUser();
         setFilterModalVisible(false)
     }
-
+    const handleRefreshFilters = () => {
+        setMaritalStatus('');
+        setHasSalah('');
+        setHasSawm('');
+        setFilterModalVisible(false);
+    };
     const handleRefresh = useCallback(() => {
         setRefreshing(true);
         setPage(1);
@@ -180,8 +184,8 @@ const Home = () => {
                 name: searchQuery
             }
             try {
-                const response = await axios.get('/search-filter', { params: filter }); // Use axios to send the request
-                setSuggestedUser(response.data.users); // Assuming the response has users array
+                const response = await axios.get('/search-filter', { params: filter });
+                setSuggestedUser(response.data.users);
                 setLoading(false);
             } catch (error) {
                 console.error(error);
@@ -220,111 +224,37 @@ const Home = () => {
             setRefreshing(false);
         }
     }, [refreshing]);
-
+    const [active, setActive] = useState('');
     useEffect(() => {
         handlegGetUnseenMessageCount();
     }, [handlegGetUnseenMessageCount]);
 
     return (
-        <SafeAreaView>
-            <Appbar.Header style={{
-                backgroundColor: '#fde8f1', shadowColor: '#000000',
-                shadowOffset: { width: 0, height: 8 },
-                shadowOpacity: 0.8,
-                shadowRadius: 2,
-                elevation: 5,
-            }}>
-                <Appbar.Content title="Muslim Matrimony" titleStyle={{ color: "#E71B73", fontFamily: "cursive", fontSize: 24, fontWeight: 'bold' }} />
-                <Appbar.Action icon="filter" onPress={showFilterModal} />
+        <>
+            <View style={{ flex: 1 }}>
+                <Appbar.Header
+                    style={{
+                        backgroundColor: '#fde8f1',
+                        shadowColor: '#000000',
+                        shadowOffset: { width: 0, height: 8 },
+                        shadowOpacity: 0.8,
+                        shadowRadius: 2,
+                        elevation: 5,
+                    }}>
+                    <Appbar.Content
+                        title="Muslim Matrimony"
+                        titleStyle={{ color: "#E71B73", fontFamily: "cursive", fontSize: 24, fontWeight: 'bold' }} />
+                    <TouchableOpacity onPress={toggleDrawer}>
+                        <Appbar.Action icon="account-filter" size={26} />
+                    </TouchableOpacity>
+                    <Badge visible={messageSeenCount > 0} size={16} style={{ position: 'absolute', top: 5, right: 5 }}>
+                        {messageSeenCount}
+                    </Badge>
+                    <Appbar.Action icon="chat-processing" onPress={routeToChatList} />
+                    <Appbar.Action icon="bell" onPress={routeToNotificationList} />
+                </Appbar.Header>
 
-                <Portal>
-                    <Modal visible={filterModalVisible} onRequestClose={hideFilterModal} transparent>
-
-                        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
-                            <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', width: '90%', backgroundColor: 'white', paddingHorizontal: 10, borderTopRightRadius: 20, borderTopLeftRadius: 20, paddingTop: 5 }}>
-                                <Text style={{ fontSize: 25, color: '#E71B73', fontWeight: 'bold', marginBottom: 10 }}>Filter Options</Text>
-                                <View style={{ flexDirection: 'row', gap: 2 }}>
-
-                                    <IconButton
-                                        icon="refresh"
-                                        iconColor='white'
-                                        size={20}
-                                        style={{ borderRadius: 20, backgroundColor: "#E71B73", }}
-                                        onPress={handleRefreshFilters} // Add onPress handler for refreshing
-                                    />
-
-                                    <IconButton
-                                        icon="magnify"
-                                        size={20}
-                                        iconColor='white'
-                                        onPress={hideFilterModal}
-                                        style={{ borderRadius: 20, backgroundColor: "#E71B73", }}
-                                    />
-                                </View>
-
-                            </View>
-                            <View style={{ backgroundColor: 'white', padding: 10, width: '90%', height: '50%', borderBottomLeftRadius: 20, borderBottomRightRadius: 20 }}>
-
-
-
-                                <ScrollView>
-
-
-                                    {/* Marital Status */}
-                                    <Text style={{ color: "#E71B73", fontWeight: "700", fontSize: 15, marginLeft: 5, marginTop: 5 }}>Marital Status</Text>
-                                    <Checkbox.Item label="Married" status={maritalStatus.includes("MARRIED") ? 'checked' : 'unchecked'} onPress={() => setMaritalStatus("MARRIED")} />
-                                    <Checkbox.Item label="Divorced" status={maritalStatus.includes("DIVORCED") ? 'checked' : 'unchecked'} onPress={() => setMaritalStatus("DIVORCED")} />
-                                    <Checkbox.Item label="Unmarried" status={maritalStatus.includes("UNMARRIED") ? 'checked' : 'unchecked'} onPress={() => setMaritalStatus("UNMARRIED")} />
-                                    <Checkbox.Item label="Partner Death" status={maritalStatus.includes("PARTNER DEATH") ? 'checked' : 'unchecked'} onPress={() => setMaritalStatus("PARTNER DEATH")} />
-
-
-                                    {/* Salah */}
-                                    <Text style={{ color: "#E71B73", fontWeight: "700", fontSize: 15, marginLeft: 5 }}>Has Salah</Text>
-                                    <Checkbox.Item label="Yes" status={hasSalah.includes("YES") ? 'checked' : 'unchecked'} onPress={() => setHasSalah("YES")} />
-                                    <Checkbox.Item label="No" status={hasSalah.includes("NO") ? 'checked' : 'unchecked'} onPress={() => setHasSalah("NO")} />
-
-                                    {/* Sawm */}
-                                    <Text style={{ color: "#E71B73", fontWeight: "700", fontSize: 15, marginLeft: 5 }}>Has Sawm</Text>
-                                    <Checkbox.Item label="Yes" status={hasSawm.includes("YES") ? 'checked' : 'unchecked'} onPress={() => setHasSawm("YES")} />
-                                    <Checkbox.Item label="No" status={hasSawm.includes("NO") ? 'checked' : 'unchecked'} onPress={() => setHasSawm("NO")} />
-
-                                    {/* <Text style={{ color: "#E71B73", fontWeight: "700", fontSize: 15, marginLeft: 5 }}>Age</Text>
-                                    <Text style={{ color: "#E71B73", fontWeight: "700", fontSize: 15, marginLeft: 5, marginTop: 5 }}>Selected Age Range: {getAgeRange()}</Text>
-                                    <MultiSlider
-                                        style={{ width: 200, height: 40 }}
-                                        minimumValue={0}
-                                        maximumValue={80}
-                                        minimumTrackTintColor="#FFFFFF"
-                                        maximumTrackTintColor="#000000"
-                                        value={Math.round(sliderValue)}
-                                        onValueChange={handleSliderChange}
-                                    /> */}
-                                    {/* <Button mode="contained" onPress={applyFilters} style={{ marginTop: 20 }}>Apply Filters</Button> */}
-
-                                </ScrollView>
-                            </View>
-                        </View>
-                    </Modal>
-                </Portal>
-                <View>
-                    {
-                        messageSeenCount ?
-                            <Badge
-                                visible={true}
-                                size={16}
-                                style={{ position: 'absolute', top: 5, right: 5 }}
-                            >
-                                {messageSeenCount}
-                            </Badge>
-                            : null
-                    }
-
-                    <Appbar.Action icon="chat" onPress={routeToChatList} />
-                </View>
-                <Appbar.Action icon="bell-outline" onPress={routeToNotificationList} />
-            </Appbar.Header>
-            {
-                isSearch ?
+                {isSearch && (
                     <View style={{ flexDirection: 'row', alignItems: 'center' }}>
                         <Searchbar
                             style={{
@@ -339,12 +269,12 @@ const Home = () => {
                             value={searchQuery}
                             onSubmitEditing={handleSearch}
                         />
-                    </View> : null
-            }
-            {loading ? (
-                <ActivityIndicator size="large" color="#E71B73" style={{ marginTop: 20 }} />
-            ) : (
-                <>
+                    </View>
+                )}
+
+                {loading ? (
+                    <ActivityIndicator size="large" color="#E71B73" style={{ marginTop: 20 }} />
+                ) : (
                     <FlatList
                         ref={flatListRef}
                         refreshControl={<RefreshControl refreshing={refreshing} onRefresh={handleRefresh} />}
@@ -355,19 +285,27 @@ const Home = () => {
                         keyExtractor={(user, index) => `${index}`}
                         ListFooterComponent={SmallLoader}
                     />
-                    {
-                        topIcon ?
-                            <View style={{ position: 'absolute', bottom: 80, right: 16, backgroundColor: '#E71B73', padding: 0, borderRadius: 50 }}>
-                                <Tooltip title="Selected Camera">
-                                    <IconButton icon="arrow-up" size={30} iconColor='white' onPress={handleScrollToTop} />
-                                </Tooltip>
-                            </View> : null
-                    }
-                </>
-            )}
-        </SafeAreaView>
+                )}
+                {topIcon && (
+                    <View style={{ position: 'absolute', bottom: 80, right: 16, backgroundColor: '#E71B73', padding: 0, borderRadius: 50 }}>
+                        <Tooltip title="Selected Camera">
+                            <IconButton icon="arrow-up" size={30} iconColor='white' onPress={handleScrollToTop} />
+                        </Tooltip>
+                    </View>
+                )}
+            </View>
 
+            {drawerVisible && <View style={globalStyles.overlay} />}
+            <Animated.View style={{ position: 'absolute', top: 0, right: drawerVisible ? 0 : -80, width: '100%', height: '100%', transform: [{ translateX }], paddingLeft: 10 }}>
+                <View style={{ backgroundColor: 'white', position: 'absolute', right: 0, width: '80%', height: '100%' }}>
+                    <FilterDrawer toggleDrawer={toggleDrawer} applyFilters={hideFilterModal} />
+                </View>
+            </Animated.View>
+
+
+        </>
     )
 }
 
 export default Home;
+
